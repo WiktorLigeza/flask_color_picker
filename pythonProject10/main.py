9effb6238ca0a3ddaf302837b18f5e0b20ba853d
WiktorLigeza <wiktorligeza@gmail.com>
2020-12-23 01:27:36 +0100

    user_login_logout_reg

--- pythonProject10/main.py
+++ pythonProject10/main.py
@@ -1,27 +1,99 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
+from user_management import *
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+import os
+from functools import wraps
 
 
+# Init app
+app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+# Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# Init db
+db = SQLAlchemy(app)
+# Init ma
+ma = Marshmallow(app)
 
 app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+# Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'user.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# Init db
+db = SQLAlchemy(app)
+# Init ma
+ma = Marshmallow(app)
+
+
+# User Class/Model
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100), unique=True)
+    username = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(25))
+
+    def __init__(self, name, email, username, password):
+        self.name = name
+        self.email = email
+        self.username = username
+        self.password = password
+
+
+# User Schema
+class UserSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'name', 'email', 'username', 'password')
+
+
+# Init schema
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
+
+
+# Check if user logged in
+def is_logged_in(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('Unauthorized, Please login', 'danger')
+            return redirect(url_for('login'))
+    return wrap
 
 
 @app.route('/')
 def index():
     return render_template('home.html')
 
-@app.route('/login')
-def log_in():
-    return render_template('login.html')
 
-@app.route('/register')
-def sign_up():
-    return render_template('register.html')
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm(request.form)
+    return user_registration(form = form, db = db)
 
-@app.route("/test", methods=["POST"])
-def test():
-    name_of_slider = request.form["value"]
-    return name_of_slider
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    return user_log_in(session=session)
+
+
+@app.route("/dashboard")
+@is_logged_in
+def dashboard():
+    return render_template('dashboard.html')
+
+
+@app.route('/logout')
+@is_logged_in
+def logout():
+    return user_log_out(session = session)
 
 
 if __name__ == '__main__':
+    app.secret_key = 'jpgmd'
     app.run(debug=True)
---
+++ pythonProject10/user_management.py
@@ -0,0 +1,65 @@
+from passlib.hash import sha256_crypt
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from main import User,render_template, request, redirect, flash, url_for
+
+
+
+
+
+# Register Form Class
+class RegisterForm(Form):
+    name = StringField('Name', [validators.Length(min=1, max=50)])
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    email = StringField('Email', [validators.Length(min=6, max=50)])
+    password = PasswordField('Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords do not match')
+    ])
+    confirm = PasswordField('Confirm Password')
+
+def user_registration(form, db):
+    if request.method == 'POST' and form.validate():
+        name = form.name.data
+        email = form.email.data
+        username = form.username.data
+        password = sha256_crypt.encrypt(str(form.password.data))
+
+        new_user = User(name, email, username, password)
+
+        db.session.add(new_user)
+        db.session.commit()
+        flash('You are now registered and can log in', 'success')
+
+        return redirect(url_for('log_in'))
+    return render_template('register.html', form=form)
+
+def user_log_in(session):
+    if request.method == 'POST':
+        # Get Form Fields
+        username = request.form['username']
+        password_candidate = request.form['password']
+
+        result = User.query.filter(User.email.endswith(username)).all()
+
+        if len(result) > 0:
+            password = result[0].password
+            if sha256_crypt.verify(password_candidate, password):
+                # Passed
+                session['logged_in'] = True
+                session['username'] = username
+
+                flash('You are now logged in', 'success')
+                return redirect(url_for('dashboard'))
+            else:
+                error = 'Invalid password'
+                return render_template('login.html', error=error)
+        else:
+            error = 'Username not found'
+            return render_template('login.html', error=error)
+
+    return render_template('login.html')
+
+def user_log_out(session):
+    session.clear()
+    flash('You are now logged out', 'success')
+    return redirect(url_for('login'))
